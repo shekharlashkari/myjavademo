4dc4bdc6a3bf8a843b2d1c911fe12bcb7a7f131a
shekharlashkari <31889713+shekharlashkari@users.noreply.github.com>
2018-03-28 22:25:10 +0530

    Create DataObject.java

---
+++ demo/filehandling/PdfFileFrom_Dzone.com/DataObject.java
@@ -0,0 +1,24 @@
+package com.test.pdf;
+public class DataObject {
+    private String year;
+    private String income;
+    private String comanyName;
+    public String getYear() {
+        return year;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+    public String getIncome() {
+        return income;
+    }
+    public void setIncome(String income) {
+        this.income = income;
+    }
+    public String getComanyName() {
+        return comanyName;
+    }
+    public void setComanyName(String comanyName) {
+        this.comanyName = comanyName;
+    }
+}
